e72763e3e91ddf376d6c4f870e387e094c71a602
sohan724code <sohanmd1375@gmail.com>
2022-06-18 00:42:56 +0600

    code updated in many files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Login } from "@mui/icons-material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hotel from "./Components/Hotel/Hotel";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 import Home from "./Pages/Home/Home";
 import List from "./Pages/Home/List/List";
 import Hotels from "./Pages/Hotels/Hotels";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="list" element={<List />} />
         <Route path="hotels" element={<Hotels />} />
         <Route path="hotels/:id" element={<Hotel />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
